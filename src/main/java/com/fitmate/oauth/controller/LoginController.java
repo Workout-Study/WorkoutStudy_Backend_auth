7b59be35decaa8f87c855c7d751e5e6916e900f5
changyu-park <cgpark@sinsiway.com>
2024-04-11 07:16:44 +0900

    Update LoginController.java

--- src/main/java/com/fitmate/oauth/controller/LoginController.java
+++ src/main/java/com/fitmate/oauth/controller/LoginController.java
@@ -1,20 +1,21 @@
 package com.fitmate.oauth.controller;
 
-import com.fitmate.oauth.service.KakaoOauthService;
-import com.fitmate.oauth.service.NaverOauthService;
+import com.fitmate.oauth.dto.naver.LoginResDto;
+import com.fitmate.oauth.service.KakaoLoginService;
+import com.fitmate.oauth.service.NaverLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
-    private final NaverOauthService naverOauthService;
-    private final KakaoOauthService kakaoOauthService;
+    private final NaverLoginService naverLoginService;
+    private final KakaoLoginService kakaoLoginService;
 
     @Autowired
-    public LoginController(NaverOauthService naverOauthService, KakaoOauthService kakaoOauthService) {
-        this.naverOauthService = naverOauthService;
-        this.kakaoOauthService = kakaoOauthService;
+    public LoginController(NaverLoginService naverLoginService, KakaoLoginService kakaoLoginService) {
+        this.naverLoginService = naverLoginService;
+        this.kakaoLoginService = kakaoLoginService;
     }
 
     /**
@@ -22,21 +23,36 @@ public class LoginController {
      * @param code 인가 코드
      * @return ResponseEntity
      */
-//    @GetMapping("/auth/login/naver")
-//    public ResponseEntity<LoginResDto> naverLogin(@RequestParam String code) {
-//
-//        return ResponseEntity.ok(result);
-//    }
+    @GetMapping("/auth/login/naver")
+    public ResponseEntity<LoginResDto> naverLogin(@RequestParam String code) {
+        LoginResDto result = naverLoginService.login(code);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/auth/logout/naver")
+    public ResponseEntity<?> naverLogout(@RequestParam String AccessToken) {
+        boolean result = naverLoginService.logout(AccessToken);
+
+        return ResponseEntity.ok().build();
+    }
 
     /**
      * 카카오 로그인
      * @param code 인가 코드
      * @return ResponseEntity
      */
-//    @GetMapping("/auth/login/kakao")
-//    public ResponseEntity<LoginResDto> kakaoLogin(@RequestParam String code) {
-//        LoginResDto result = kakaoOauthService.getKakaoToken(code);
-//
-//        return ResponseEntity.ok(result);
-//    }
+    @GetMapping("/auth/login/kakao")
+    public ResponseEntity<LoginResDto> kakaoLogin(@RequestParam String code) {
+        LoginResDto result = kakaoLoginService.login(code);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/auth/logout/naver")
+    public ResponseEntity<?> kakaoLogout(@RequestParam String AccessToken) {
+        boolean result = kakaoLoginService.logout(AccessToken);
+
+        return ResponseEntity.ok().build();
+    }
 }
