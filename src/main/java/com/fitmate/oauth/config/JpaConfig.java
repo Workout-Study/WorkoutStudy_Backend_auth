db2bbcf2916da2c3a6e7a83535072f4d152b2528
Kimsunfang <kimsunfang@gmail.com>
2024-05-19 21:11:54 +0900

    FIX docker compose 7

--- src/main/java/com/fitmate/oauth/config/JpaConfig.java
+++ src/main/java/com/fitmate/oauth/config/JpaConfig.java
@@ -1,4 +1,37 @@
 package com.fitmate.oauth.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
 public class JpaConfig {
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setDataSource(dataSource);
+		factoryBean.setPackagesToScan("com.fitmate.oauth.jpa.entity"); // 엔티티 클래스 패키지
+		factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		return factoryBean;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactoryBean(LocalContainerEntityManagerFactoryBean factoryBean) {
+		return factoryBean.getObject();
+	}
 }
