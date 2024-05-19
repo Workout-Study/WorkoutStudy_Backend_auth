ed3cc68192a25d0a227e3962eb7311682bfdae34
Kimsunfang <kimsunfang@gmail.com>
2024-05-19 21:47:05 +0900

    FIX docker compose 13

--- src/main/java/com/fitmate/oauth/OauthApplication.java
+++ src/main/java/com/fitmate/oauth/OauthApplication.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EnableTransactionManagement
-@EnableJpaAuditing
 public class OauthApplication {
 
 	public static void main(String[] args) {
