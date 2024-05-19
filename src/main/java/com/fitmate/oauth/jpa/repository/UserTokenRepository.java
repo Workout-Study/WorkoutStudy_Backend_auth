4f59cacefe44f73e610adc827981da7d73029f57
Kimsunfang <kimsunfang@gmail.com>
2024-05-19 21:49:03 +0900

    FIX docker compose 14

--- src/main/java/com/fitmate/oauth/jpa/repository/UserTokenRepository.java
+++ src/main/java/com/fitmate/oauth/jpa/repository/UserTokenRepository.java
@@ -2,9 +2,10 @@ package com.fitmate.oauth.jpa.repository;
 
 import com.fitmate.oauth.jpa.entity.UserToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserTokenRepository extends JpaRepository<UserToken, Long> {
     UserToken findByUserId(long userId);
     void deleteUserTokenByAccessToken(String accessToken);
--- src/main/java/com/fitmate/oauth/jpa/repository/UsersRepository.java
+++ src/main/java/com/fitmate/oauth/jpa/repository/UsersRepository.java
@@ -2,7 +2,9 @@ package com.fitmate.oauth.jpa.repository;
 
 import com.fitmate.oauth.jpa.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
     Users findByUserId(Long userId);
