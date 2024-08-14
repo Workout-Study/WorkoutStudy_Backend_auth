5f6e0a52f8c41f2ff4b6956cea35ff29fe1e044c
username <57055730+Jake-huen@users.noreply.github.com>
2024-08-14 21:47:21 +0900

    FEAT : Time 형식 변환 조건 구현

---
+++ src/main/java/com/fitmate/oauth/util/TimeUtils.java
@@ -0,0 +1,15 @@
+package com.fitmate.oauth.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeUtils {
+    public static String formatTimeToCustomString(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        ZonedDateTime zonedDateTime = dateTime.atZone(ZoneId.of("UTC+09"));
+        String formattedDateTime = zonedDateTime.format(formatter);
+        return formattedDateTime + "+09";
+    }
+}
