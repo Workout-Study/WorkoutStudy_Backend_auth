b2bbc2112851cc499da0e8207a13213760f308b9
username <57055730+Jake-huen@users.noreply.github.com>
2024-08-19 10:17:14 +0900

    FEAT Instant -> TimeStamp

--- src/main/java/com/fitmate/oauth/util/TimeUtils.java
+++ src/main/java/com/fitmate/oauth/util/TimeUtils.java
@@ -1,5 +1,6 @@
 package com.fitmate.oauth.util;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -11,5 +12,11 @@ public class TimeUtils {
         ZonedDateTime zonedDateTime = dateTime.atZone(ZoneId.of("UTC+09"));
         String formattedDateTime = zonedDateTime.format(formatter);
         return formattedDateTime;
+    }
+
+    public static String formatInstantToTimestamp(Instant instant) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSSXXX")
+                .withZone(ZoneId.systemDefault());
+        return formatter.format(instant);
     }
 }
